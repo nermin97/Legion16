3ab9b667c9a53155300c5af6c161b7636f93b538
nerminKrdzic <nermin.krdzic@pragmatio.com>
2020-12-19 15:27:47 +0100

    Created project

---
+++ cmake-build-debug/CMakeFiles/Legion16.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Legion16.dir/main.cpp.obj"
+  "Legion16.exe"
+  "Legion16.exe.manifest"
+  "Legion16.lib"
+  "Legion16.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Legion16.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
