88b93813bbb88bae1c25ad52a2115377df5dcd4e
Pavo <pzornija1@etf.unsa.ba>
2020-12-19 16:53:58 +0100

    Set up development environment for emulator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <fstream>
+//#include <bits/stdc++.h>
+#include <sys/stat.h>
+#include<string>
+#include<bitset>
+#include<dirent.h>
+#include<vector>
+#define _POSIX_SOURCE
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+#undef _POSIX_SOURCE
+#include <stdio.h>
+#include<sstream>
+#include <cmath>
+
+void emulate() {
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    emulate();
+    std::cout << "Emulation finished!" << std::endl;
     return 0;
 }
