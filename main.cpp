ac5bbdfe9c8178ba52248c8c1cb6f0a2a926eee8
Pavo <pzornija1@etf.unsa.ba>
2020-12-24 16:28:43 +0100

    Generating random instructions

--- main.cpp
+++ main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 #include <fstream>
-//#include <bits/stdc++.h>
+#include <bits/stdc++.h>
 #include <sys/stat.h>
 #include<string>
-#include<bitset>
-#include<dirent.h>
-#include<vector>
-//#define _POSIX_SOURCE
-//#include <fcntl.h>
-//#include <sys/types.h>
-//#include <unistd.h>
-//#undef _POSIX_SOURCE
-//#include <stdio.h>
-//#include<sstream>
-//#include <cmath>
+#include <bitset>
+#include <dirent.h>
+#include <vector>
+#define _POSIX_SOURCE
+#include <fcntl.h>
+#include <sys/types.h>
+#include <unistd.h>
+#undef _POSIX_SOURCE
+#include <stdio.h>
+#include <sstream>
+#include <cmath>
+#include <time.h>
 
 using namespace std;
 
-unsigned int mem[65536]={0x0000}, registers[16], noOfInstructions; // regs[15] je PC
+unsigned short int memory[65536]={0x0000}, registers[16], noOfInstructions; // regs[15] je PC
 int fileModes[16], openModes[16], lseekpos[16], fd;
 
 void populateOpenModes(){
@@ -92,11 +93,34 @@ void populateLseeekPos(){
 }
 
 void emulate() {
-    cout << mem << std::endl;
+    int i = 0;
+    unsigned short int opcode, r, n, k;
+    bool r1, n1;
+    memory[0] = 366;
+    opcode = memory[0];
+
+    switch(opcode & 0xF000) {
+        case 0x0000:
+            cout << "jeste, tu smo stigli " << std::endl;
+            break;
+        default:
+            cout << "nije to to, u default smo" << std::endl;
+    }
+//    opcode
+//    cout << mem << std::endl;
 }
 
 void generateInstructionsRandomly () {
+    // We use this to generate random instructions and number of instructions
+    srand (time(NULL));
+    unsigned short int numberOfInstructions = rand() % 1000 + 100;
 
+    for(int i = 0; i < numberOfInstructions; ++i)
+        memory[i] = rand() % 65536 + 0;
+//    cout << "prvi " << memory[0] << " broj inst " << numberOfInstructions << std::endl;
+
+//    mem
+//    cout << instruction << " int verzija" << std::hex << instruction << " instrukcija" << std::endl;
 }
 
 void loadInstructions() {
@@ -124,8 +148,10 @@ int main() {
 //    loadInstructions(); //Očitavamo instrukcije iz mem.txt
 //
 //    interpret();
-    loadInstructions();
-//    emulate();
+//    loadInstructions();
+//    generateInstructionsRandomly();
+    emulate();
+    generateInstructionsRandomly();
 //    std::cout << "Emulation finished!" << std::endl;
     return 0;
 }
