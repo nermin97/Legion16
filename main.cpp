f5233454801c25ac01c2d74c6699d0b2b7aa951d
Pavo <pzornija1@etf.unsa.ba>
2020-12-26 22:21:29 +0100

    All instructions except system calls

--- main.cpp
+++ main.cpp
@@ -99,6 +99,11 @@ void populateLseeekPos(){
 }
 
 void emulate() {
+    // Program counters
+    unsigned int TPC = 0, SPC = 0;
+    unsigned char RT, RA;
+    unsigned short int r, n, k;
+    bool rBOOL, nBOOL;
     // We need here code for specific opcode, dest, src1 and src2
     // Used to generate random instructions and a random number of those
 
@@ -115,7 +120,7 @@ void emulate() {
     for(int i = 0; i < numberOfInstructions; ++i) {
         memory[i] = rand() % 65536;
 
-        // decide what routine it is to which to jump
+        // decide what routine it is to which to jump to
         switch(memory[i] & 0xF000) {
             case 0x0000:
                 code[i].opcode = &&LOD;
@@ -172,85 +177,155 @@ void emulate() {
         code[i].src2 = memory[i] & 0x000F;
     }
 
-    unsigned int TPC = 0, SPC = 0;
-    unsigned char RT, RA;
+    // Check if there is no data available
+    if(numberOfInstructions == 0 || sizeof(memory) == 0 || sizeof(code) == 0) goto EXIT;
+
+    // Initial jump to first routine
+    goto *code[0].opcode;
+
     // Here are our routines defined
     LOD:
-        registers[code[TPC].dest] = code[TPC].src2;
+        // Ovdje treba ja mislim memory[code[TPC].src2] (ja mislim da je greska u proslom kodu)
+        registers[code[TPC].dest] = registers[code[TPC].src2];
+        SPC += 4;
+        TPC++;
+        if(TPC == numberOfInstructions) goto EXIT;
+        goto *code[TPC].opcode;
     ADD:
-
+        registers[code[TPC].dest] = registers[code[TPC].src1] + registers[code[TPC].src2];
+        SPC += 4;
+        TPC++;
+        if(TPC == numberOfInstructions) goto EXIT;
+        goto *code[TPC].opcode;
     SUB:
-
+        registers[code[TPC].dest] = registers[code[TPC].src1] - registers[code[TPC].src2];
+        SPC += 4;
+        TPC++;
+        if(TPC == numberOfInstructions) goto EXIT;
+        goto *code[TPC].opcode;
     AND:
-
+        registers[code[TPC].dest] = registers[code[TPC].src1] & registers[code[TPC].src2];
+        SPC += 4;
+        TPC++;
+        if(TPC == numberOfInstructions) goto EXIT;
+        goto *code[TPC].opcode;
     ORA:
-
+        registers[code[TPC].dest] = registers[code[TPC].src1] | registers[code[TPC].src2];
+        SPC += 4;
+        TPC++;
+        if(TPC == numberOfInstructions) goto EXIT;
+        goto *code[TPC].opcode;
     XOR:
-
+        registers[code[TPC].dest] = registers[code[TPC].src1] ^ registers[code[TPC].src2];
+        SPC += 4;
+        TPC++;
+        if(TPC == numberOfInstructions) goto EXIT;
+        goto *code[TPC].opcode;
     SHR:
+        r = registers[code[TPC].src2];
+        n = registers[code[TPC].src2] & 0x000F;
+        if (((r >> 4) & 0x0003) == 0) {
+            k = (registers[code[TPC].src1] | 0x7FFF);
+            // odredjivanje je li 1 najveci bit, ako je 1 mora se dodati 1 na kraj, a ako je 0, obicni shift ce svakako dodati nulu
+            registers[code[TPC].dest] = k == 0xFFFF ?  (registers[code[TPC].src1] >> n) & ~(((0x1 << 16) >> n) << 1) : registers[code[TPC].src1] >> n;
+        }
+        else if (((r >> 4) & 0x0003) == 1)
+            registers[code[TPC].dest] = registers[code[TPC].src1] >> n;
+        else if (((r >> 4) & 0x0003) == 2)
+            registers[code[TPC].dest] = (registers[code[TPC].src1] << n) | (registers[code[TPC].src1] >> (16 - n));
+        else
+            registers[code[TPC].dest] = registers[code[TPC].src1] << n;
 
+        SPC += 4;
+        TPC++;
+        if(TPC == numberOfInstructions) goto EXIT;
+        goto *code[TPC].opcode;
     MUL:
-
+        registers[code[TPC].dest] = registers[code[TPC].src1] * registers[code[TPC].src2];
+        SPC += 4;
+        TPC++;
+        if(TPC == numberOfInstructions) goto EXIT;
+        goto *code[TPC].opcode;
     STO:
-
+        memory[registers[code[TPC].src2]] = registers[code[TPC].src1];
+        registers[code[TPC].dest] = registers[code[TPC].src1];
+        SPC += 4;
+        TPC++;
+        if(TPC == numberOfInstructions) goto EXIT;
+        goto *code[TPC].opcode;
     LDC:
-
+        registers[code[TPC].dest] = (code[TPC].src1 | code[TPC].src2);
+        SPC += 4;
+        TPC++;
+        if(TPC == numberOfInstructions) goto EXIT;
+        goto *code[TPC].opcode;
     GTU:
-
+        registers[code[TPC].dest] = registers[code[TPC].src1] > registers[code[TPC].src2] ? 0x0001 : 0x0000;
+        SPC += 4;
+        TPC++;
+        if(TPC == numberOfInstructions) goto EXIT;
+        goto *code[TPC].opcode;
     GTS:
+        r = registers[code[TPC].src1];
+        n = registers[code[TPC].src2];
+        rBOOL = (r & 0x8000) == 0x0000 ? false : true;
+        nBOOL = (n & 0x8000) == 0x0000 ? false : true;
+        if(rBOOL && !nBOOL)
+            registers[code[TPC].dest] = 0x0001;
+        else if(!rBOOL && nBOOL)
+            registers[code[TPC].dest] = 0x0000;
+        else
+            registers[code[TPC].dest] = registers[code[TPC].src1] > registers[code[TPC].src2] ? 0x0001 : 0x0000;
 
+        SPC += 4;
+        TPC++;
+        if(TPC == numberOfInstructions) goto EXIT;
+        goto *code[TPC].opcode;
     LTU:
-
+        registers[code[TPC].dest] = registers[code[TPC].src1] < registers[code[TPC].dest] ? 0x0001 : 0x0000;
+        SPC += 4;
+        TPC++;
+        if(TPC == numberOfInstructions) goto EXIT;
+        goto *code[TPC].opcode;
     LTS:
+        r = registers[code[TPC].src1];
+        n = registers[code[TPC].src2];
+        rBOOL = (r & 0x8000) == 0x0000 ? false : true;
+        nBOOL = (n & 0x8000) == 0x0000 ? false : true;
+        if(rBOOL && !nBOOL)
+            registers[code[TPC].dest] = 0x0000;
+        else if(!rBOOL && nBOOL)
+            registers[code[TPC].dest] = 0x0001;
+        else
+            registers[code[TPC].dest] = registers[code[TPC].src1] < registers[code[TPC].src2] ? 0x0001 : 0x0000;
 
+        SPC += 4;
+        TPC++;
+        if(TPC == numberOfInstructions) goto EXIT;
+        goto *code[TPC].opcode;
     EQU:
-
+        registers[code[TPC].dest] = registers[code[TPC].src1] == registers[code[TPC].src2] ? 0x0001 : 0x0000;
+        SPC += 4;
+        TPC++;
+        if(TPC == numberOfInstructions) goto EXIT;
+        goto *code[TPC].opcode;
     MAJ:
 
     EXIT:
         memory[65536] = {0x0000};
+        n = 0;
+        k = 0;
+        r = 0;
+        TPC = 0;
+        SPC = 0;
+        cout << "End of emulation!" << std::endl;
 }
 
-//void decideRoutine(unsigned short int mem, int i) {
-//    switch(mem & 0xF000) {
-//        case 0x0000:
-//            code[i].opcode2 = &&LOD;
-//    }
-//}
-//
-//// PREBACITI OVO SVE U emulate()
-//void generateInstructionsRandomly () {
-//    // We need here code for specific opcode, dest, src1 and src2
-//    // Used to generate random instructions and a random number of those
-//
-//    // This won't work exactly because we need to watch out for instruction forms that don't match any appropriate specific instruction format, maybe we will have to manually prepare it
-//    // instr format OP|DEST|SRC1|SRC2
-////    cout << " da probam jos nesto " << ((65535) & 0x000F) <<" primjer hex instr " << std::hex << 65535 << " pomjereno do prvog " << ((0xFABC) & 0x000F) << std::endl;
-////    (65535 & 0xF000) dobijanje prvog opcode
-////    ((0xFABC) & 0x000F) src2
-////    ((0xFABC >> 8) & 0x000F) dest
-////    ((0xFABC >> 4) & 0x000F) src1
-//    srand (time(NULL));
-//
-//    numberOfInstructions = rand() % 1000 + 100;
-//    for(int i = 0; i < numberOfInstructions; ++i) {
-//        memory[i] = rand() % 65536;
-////        code[i].opcode1 = "ROUTINE" + to_string(memory[i] & 0xF000);
-//        decideRoutine(memory[i], i);
-//        code[i].dest = (memory[i] >> 8) & 0x000F;
-//        code[i].src1 = (memory[i] >> 4) & 0x000F;
-//        code[i].src2 = memory[i] & 0x000F;
-//    }
-//}
-
 int main() {
-//    settingRegistersToZero();
-//    populateOpenModes();
-//    populateLseeekPos();
-//    populateFileModes();
-    generateInstructionsRandomly();
-//    emulate();
-
-//    std::cout << "Emulation finished!" << std::endl;
+    settingRegistersToZero();
+    populateOpenModes();
+    populateLseeekPos();
+    populateFileModes();
+    emulate();
     return 0;
 }
